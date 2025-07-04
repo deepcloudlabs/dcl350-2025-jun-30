a2a6658a21030a40e5737284a4c558fea6014d2f
Binnur KURT <deepcloudlabs@gmail.com>
2025-07-04 12:27:38 +0300

    initial commit

--- crm-reactive-microservice/src/main/java/com/example/crm/repository/CustomerReactiveRepository.java
+++ crm-reactive-microservice/src/main/java/com/example/crm/repository/CustomerReactiveRepository.java
@@ -1,6 +1,8 @@
 package com.example.crm.repository;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.example.crm.document.Customer;
@@ -11,4 +13,6 @@ import reactor.core.publisher.Mono;
 public interface CustomerReactiveRepository extends ReactiveMongoRepository<Customer, String>{
 	Flux<Customer> findAllByAddressesCountry(Pageable page,String country);
 	Mono<Customer> findOneByEmail(String email);
+	@Query("{}")
+	Flux<Customer> findAll(PageRequest page);
 }
